a3807e1e0c822c8747d8213dea581c31fe1bbc04
AarambhaAnta <aarambha108@gmail.com>
2025-11-05 16:10:48 +0530

    feat: understand variable decarations

---
+++ 3_variables/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// var name string = "golang"
+	var age int = 30
+
+	// infer
+	// var name = "golang"
+	var isAdult = true
+
+	// shorthand syntax
+	// name := "golang"
+
+	var name string
+	name = "golang"
+
+	// var price float32 = 50.5
+	// var price = 50.5
+	price := 50.5
+
+	fmt.Println(name)
+	fmt.Println(isAdult)
+	fmt.Println(age)
+	fmt.Println(price)
+}
