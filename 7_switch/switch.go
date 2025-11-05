c468b69f49e02da94d3755fdb46f59e6e1329b1f
AarambhaAnta <aarambha108@gmail.com>
2025-11-05 16:46:55 +0530

    feat: understand switch case

---
+++ 7_switch/switch.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// simple switch
+	// i := 1
+
+	// switch i {
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("two")
+	// case 3:
+	// 	fmt.Println("three")
+	// default:
+	// 	fmt.Println("other")
+	// }
+
+	// multiple condition switch
+
+	// switch time.Now().Weekday() {
+	// case time.Saturday, time.Sunday:
+	// 	fmt.Println("it's weekend")
+	// default:
+	// 	fmt.Println("it's workday")
+	// }
+
+	// type switch
+	whoAmI := func (i interface{})  {
+		switch t := i.(type) {
+		case int:
+			fmt.Println("it's an integer")
+		case string :
+			fmt.Println("it's an string")
+		case bool:
+			fmt.Println("it's a boolean")
+		default:
+			fmt.Println("Other", t)
+		}
+	}
+
+	whoAmI("golang")
+	whoAmI(true)
+	whoAmI(33)
+}
